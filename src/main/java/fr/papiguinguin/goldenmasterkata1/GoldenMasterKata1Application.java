e53304436c5a1ec965c1a61493b5e9eb29d64334
maxim <maxime.guinchard@hotmail.fr>
2023-01-18 05:08:52 +0100

    FIrst commit

--- src/main/java/fr/papiguinguin/goldenmasterkata1/GoldenMasterKata1Application.java
+++ src/main/java/fr/papiguinguin/goldenmasterkata1/GoldenMasterKata1Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GoldenMasterKata1Application {
+    // test
 
     public static void main(String[] args) {
         SpringApplication.run(GoldenMasterKata1Application.class, args);
